8d849c2bfbde37ac863e90552bdcb9c24b4ae82c
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-06-20 11:20:02 +0300

    configure local testing

--- src/services/hello.ts
+++ src/services/hello.ts
@@ -1,12 +1,19 @@
+import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 import { v4 } from 'uuid';
+
+const s3Client = new S3Client({});
 
 async function handler(event: APIGatewayProxyEvent, context: Context) {
     console.log(event);
 
+    const command = new ListBucketsCommand({});
+    const listBucketsResult = (await s3Client.send(command)).Buckets;
+    console.log(JSON.stringify(listBucketsResult));
+
     const response: APIGatewayProxyResult = {
         statusCode: 200,
-        body: JSON.stringify(`Hello from lambda! This is the id: ${v4()}`)
+        body: JSON.stringify(listBucketsResult)
     };
 
     return response;
