f56f32216347b873eee3ba1a9658678ab07ec3c1
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-07-11 21:50:12 +0300

    refactor tests

--- test/infra/monitor.test.ts
+++ test/infra/monitor.test.ts
@@ -3,18 +3,30 @@ import { Template } from "aws-cdk-lib/assertions";
 import { MonitorStack } from "../../src/infra/stacks/MonitorStack";
 
 describe('Initial test suite', () => {
-    test('initial test', () => {
+    let monitorStackTemplate: Template;
+
+    beforeAll(() => {
         const testApp = new App({
             outdir: 'cdk.out'
         });
         const monitorStack = new MonitorStack(testApp, 'MonitorStack');
-        const monitorStackTemplate = Template.fromStack(monitorStack);
+        monitorStackTemplate = Template.fromStack(monitorStack);
+    });
 
+    test('Lambda properties test', () => {
         monitorStackTemplate.hasResourceProperties('AWS::Lambda::Function', {
             Handler: 'index.handler',
             Runtime: 'nodejs18.x'
         });
 
-        expect(true).toBeTruthy();
+    });
+
+    test('Lambda properties test', () => {
+
+        monitorStackTemplate.hasResourceProperties('AWS::SNS::Topic', {
+            DisplayName: 'AlarmTopic',
+            TopicName: 'AlarmTopic'
+        });
+
     });
 });
